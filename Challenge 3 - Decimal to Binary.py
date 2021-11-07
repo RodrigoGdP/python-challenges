416382ce3717dc7239a345d561a582cf49c9ff44
Bjern <93327141+RodrigoGdP@users.noreply.github.com>
2021-11-07 20:55:42 -0300

    Add files via upload

--- Challenge 3 - Decimal to Binary.py
+++ Challenge 3 - Decimal to Binary.py
@@ -5,5 +5,6 @@ def dbin (num):
         bin.append(num % 2)
         num = math.floor(num / 2)
     bin.reverse()
+    bin = int("".join(map(str, bin)))
     return bin
-print (dbin(1025))
+print (dbin(10))
